155a3c537b7cf88d00bd2dc6f535ab6881eefa82
乔治 <1975657371@qq.com>
2015-07-19 15:30:42 +0800

    增加监控

--- app.js
+++ app.js
@@ -5,3 +5,12 @@
 
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 80;
+
+
+app.get('/', function (req,res) {
+    res.json({success:1});
+})
+
+
+app.listen(port);

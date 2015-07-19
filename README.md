3dfad94c36b262d279d67ef5294d2c3927b97565
乔治 <1975657371@qq.com>
2015-07-19 15:27:09 +0800

    新建项目

--- README.md
+++
@@ -1,8 +0,0 @@
-ÖÐ¼ä¼þ
-
-express-handlebars
-body-parser
-express-session
-cookie-parser
-geetest
-connect-mongo
--- app.js
+++ app.js
@@ -1,18 +1,7 @@
 /**
  *  George
- *  2015/7/19 14:51
+ *  2015/7/19 15:26
  */
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 80;
-var bodyParse = require('body-parse');
-
-
-
-app.get('/', function (req,res) {
-    res.json({success:1});
-})
-
-
-app.listen(prot);
